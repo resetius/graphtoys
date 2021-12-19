d12eeb283a1b0d5f10b858d0ab33147cedfd2746
Alexey Ozeritskiy <aozeritsky@gmail.com>
2021-12-19 20:23:44 +0300

    Program

--- program.c
+++ program.c
@@ -1,1 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "program.h"
+
+static void log_func(const char* msg) {
+    puts(msg);
+}
+
+struct Program* prog_new() {
+    struct Program* p = calloc(1, sizeof(struct Program));
+    p->program = glCreateProgram();
+    p->log = log_func;
+    return p;
+}
+
+void prog_free(struct Program* p) {
+    GLint numShaders = 0;
+    GLuint *shaders;
+    int i;
+
+    if (!p->program) {
+        return;
+    }
+
+    glGetProgramiv(p->program, GL_ATTACHED_SHADERS, &numShaders);
+    shaders = malloc(numShaders*sizeof(GLint));
+    glGetAttachedShaders(p->program, numShaders, NULL, shaders);
+
+    for (i = 0; i < numShaders; i++) {
+        glDeleteShader(shaders[i]);
+    }
+
+    glDeleteProgram(p->program);
+    free(shaders);
+}
+
+static int prog_add(struct Program* p, const char* shaderText, GLuint shader) {
+    int result;
+    glShaderSource(shader, 1, &shaderText, NULL);
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+
+    if (GL_FALSE == result) {
+        // get error log
+        int length = 0;
+        p->log("Shader compilation failed");
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+        if (length > 0) {
+            char* msg = malloc(length+1);
+            int written = 0;
+            glGetShaderInfoLog(shader, length, &written, msg);
+            p->log(msg);
+            free(msg);
+        }
+        return 0;
+    }
+    glAttachShader(p->program, shader);
+    return 1;
+}
+
+int prog_add_vs(struct Program* p, const char* shader) {
+    GLuint shaderId = glCreateShader(GL_VERTEX_SHADER);
+    return prog_add(p, shader, shaderId);
+}
+
+int prog_add_fs(struct Program* p, const char* shader) {
+    GLuint shaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    return prog_add(p, shader, shaderId);
+}
--- program.h
+++ program.h
@@ -1,21 +1,21 @@
 #pragma once
 
-enum ShaderType {
-    VERTEX,
-    FRAGMENT
-};
+#include "glad/gl.h"
 
 struct Program {
     GLuint program;
+    void (*log)(const char* msg);
 };
 
 struct Program* prog_new();
 void prog_free(struct Program* p);
 
-void prog_add(struct Program* p, const char* shader, enum ShaderType type);
-void prog_link(struct Program* p);
-void prog_use(struct Program* p);
-void prog_validate(struct Program* p);
+int prog_add_vs(struct Program* p, const char* shader);
+int prog_add_fs(struct Program* p, const char* shader);
 
-void prog_bind_attr(struct Program* p, GLuint location, const char* name);
-void prog_set_uniform(struct Program* p, const char* name);
+int prog_link(struct Program* p);
+int prog_use(struct Program* p);
+int prog_validate(struct Program* p);
+
+int prog_bind_attr(struct Program* p, GLuint location, const char* name);
+int prog_set_uniform(struct Program* p, const char* name);
