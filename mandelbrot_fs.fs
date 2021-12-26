4bfcb062749b8316fd35a9d1810ae71586f9284b
Alexey Ozeritskiy <aozeritsky@gmail.com>
2021-12-26 15:22:04 +0300

    Print net

--- mandelbrot_fs.fs
+++ mandelbrot_fs.fs
@@ -1,6 +1,7 @@
 #version 330
 // precision highp float;
 uniform mat4 MVP;
+uniform vec3 T;
 
 in vec2 coord;
 out vec4 fragColor;
@@ -28,6 +29,16 @@ void main() {
 	//Change color from HSV to RGB. Algorithm from https://gist.github.com/patriciogonzalezvivo/114c1653de9e3da6e1e3
 	vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
 	vec3 m = abs(fract(color.xxx + K.xyz) * 6.0 - K.www);
+
+    for (int i = -4; i <= 4; i++) {
+        float thin =  i == 0
+            ? 3e-3
+            : 1e-3;
+        if (abs(coord.x+i*0.5)<T.z*thin || abs(coord.y+i*0.5) < T.z*thin) {
+            fragColor = vec4(1.0,1.0,1.0,1.0);
+            return;
+        }
+    }
 
     fragColor = vec4(color.z * mix(K.xxx, clamp(m - K.xxx, 0.0, 1.0), color.y), 1.0);
 }
