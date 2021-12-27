f54d5a0e3bbcc16d8fc26404fa45864d19981796
Alexey Ozeritskiy <aozeritsky@gmail.com>
2021-12-27 20:50:20 +0300

    Create font textures

--- font/font.c
+++ font/font.c
@@ -8,15 +8,52 @@
 #include "program.h"
 #include "RobotoMono-Regular.h"
 
+struct Char {
+    GLuint tex_id;
+    wchar_t ch;
+    int w;
+    int h;
+};
+
 struct FontImpl {
     struct Font base;
     struct Mesh* m;
     struct Program* p;
+    struct Char chars[65536];
     FT_Library library;
     FT_Face face;
 };
 
-void font_render(struct Font* o, float x, float y, const char* string) {
+static void char_load(struct FontImpl* f, wchar_t ch) {
+    struct Char* out = &f->chars[ch];
+    int error;
+    error = FT_Load_Char(f->face, ch, FT_LOAD_RENDER);
+    if (error) {
+        printf("Cannot load char: %d\n", error);
+        exit(1);
+    }
+    error = FT_Render_Glyph(f->face->glyph, FT_RENDER_MODE_NORMAL);
+    if (error) {
+        printf("Cannot render glyph: %d\n", error);
+        exit(1);
+    }
+
+    FT_Bitmap bitmap = f->face->glyph->bitmap;
+
+    glGenTextures(1, &out->tex_id);
+    glBindTexture(GL_TEXTURE_2D, out->tex_id);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, bitmap.pitch);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0,
+        GL_RED,
+        bitmap.width,
+        bitmap.rows, 0,
+        GL_RED,
+        GL_UNSIGNED_BYTE,
+        bitmap.buffer);
+}
+
+static void font_render(struct Font* o, float x, float y, const char* string) {
     struct FontImpl* f = (struct FontImpl*)o;
     int error = FT_Load_Char(f->face, 'A', FT_LOAD_RENDER);
     if (error) {
@@ -27,6 +64,7 @@ void font_render(struct Font* o, float x, float y, const char* string) {
 
 struct Font* font_new() {
     struct FontImpl* t = calloc(1, sizeof(*t));
+    int i;
 
     struct Vertex1 vertices[] = {
         {{-1.0f, 1.0f,0.0f}},
@@ -76,11 +114,25 @@ struct Font* font_new() {
     prog_add_fs(t->p, font_font_fs);
     prog_link(t->p);
 
+    memset(t->chars, 0, sizeof(t->chars));
+    for (i = 'a'; i <= 'z'; i++) {
+        char_load(t, i);
+    }
+    for (i = 'A'; i <= 'Z'; i++) {
+        char_load(t, i);
+    }
+
     return (struct Font*)t;
 }
 
 void font_free(struct Font* o) {
+    int i;
     struct FontImpl* f = (struct FontImpl*)o;
+    for (i = 0; i < sizeof(f->chars)/sizeof(struct Char); i++) {
+        if (f->chars[i].ch) {
+            glDeleteTextures(1, &f->chars[i].tex_id);
+        }
+    }
     mesh_free(f->m);
     prog_free(f->p);
     free(f);
