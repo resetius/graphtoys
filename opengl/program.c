37434fc5ed1d4b100eebd51e0d247b448b7b464b
Alexey Ozeritskiy <aozeritsky@gmail.com>
2022-01-02 13:35:40 +0300

    double free

--- opengl/program.c
+++ opengl/program.c
@@ -45,7 +45,6 @@ static void prog_free_(struct Program* p1) {
     free(shaders);
 
     glDeleteProgram(p->program);
-    free(shaders);
 }
 
 static int prog_add_(struct ProgramImpl* p, const char* shaderText, GLuint shader) {
